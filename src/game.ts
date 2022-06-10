3364b1088d37710b39403de21f6c1676de8c3604
Virtuoz <chaporovag@gmail.com>
2022-06-10 08:03:33 +0300

    test poap init

--- src/game.ts
+++ src/game.ts
@@ -1,1 +1,11 @@
+import {createDispenser} from "./libs/booth/dispenser";
+
 const _scene = new Entity('_scene')
+
+createDispenser(
+    {
+        position: new Vector3(18.5,35.5, 12),
+        rotation: Quaternion.Euler(0, 0, 0)
+    },
+    'acd27e4b-24bd-4040-b715-c0e11e863fb0'
+)
